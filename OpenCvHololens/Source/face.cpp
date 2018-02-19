9e261b21221864f2176ff7a4ef038c93d14e925a
Mazin Bokhari (TME) <Mazin.Bokhari@toyota-europe.com>
2018-02-19 15:21:45 +0100

    Added some required functions

---
+++ OpenCvHololens/Source/face.cpp
@@ -0,0 +1,5 @@
+#include "face_FaceRecognizer.h"
+#include "face_BasicFaceRecognizer.h"
+#include "face_EigenFaceRecognizer.h"
+#include "face_FisherFaceRecognizer.h"
+#include "face_LBPHFaceRecognizer.h"
