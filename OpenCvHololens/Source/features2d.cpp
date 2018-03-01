4b1fa018d5b30bdf8b556ed21f1fd8a749ef834b
Firifire <firifire@gmail.com>
2018-03-01 16:36:42 +0100

    Fixed a bug with some features2d dll entry point not found

--- OpenCvHololens/Source/features2d.cpp
+++ OpenCvHololens/Source/features2d.cpp
@@ -1,6 +1,6 @@
+#include "pch.h"
 #include "features2d.h"
 #include "features2d_DescriptorMatcher.h"
-#include "pch.h"
 #include "features2d_FeatureDetector.h"
 
 #include "features2d_AgastFeatureDetector.h"
